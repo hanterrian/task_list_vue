ec6538631eaf5bd8eb428bb992754cf2ccac1ee9
hanterrian <hanterrian@gmail.com>
2024-01-17 18:58:45 +0200

    Add basic

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,7 +1,6 @@
 import {defaultConfig} from '@formkit/vue'
 import {genesisIcons} from "@formkit/icons"
 import { generateClasses } from '@formkit/tailwindcss';
-import {rootClasses} from './formkit.theme'
 import {createAutoAnimatePlugin} from '@formkit/addons'
 
 export default defaultConfig({
@@ -12,7 +11,6 @@ export default defaultConfig({
         ...genesisIcons
     },
     config: {
-        // rootClasses,
         classes: generateClasses({
             text: {
                 outer: 'mb-5',
@@ -23,6 +21,15 @@ export default defaultConfig({
                 messages: 'list-none p-0 mt-1 mb-0',
                 message: 'text-red-500 mb-1 text-xs',
             },
+            password: {
+                outer: 'mb-5',
+                label: 'block mb-1 font-bold text-sm',
+                inner: 'bg-white max-w-md border border-gray-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                input: 'w-full h-10 px-3 bg-transparent border-none focus:outline-none text-base text-gray-700 placeholder-gray-400 focus:outline-none',
+                help: 'text-xs text-gray-500',
+                messages: 'list-none p-0 mt-1 mb-0',
+                message: 'text-red-500 mb-1 text-xs',
+            }
         }),
 
     },
