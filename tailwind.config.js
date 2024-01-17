3bf50a3ba7fe9956c7a7021e2be3cdc4e10fa271
hanterrian <hanterrian@gmail.com>
2024-01-17 18:42:12 +0200

    Add basic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		`./components/**/*.{vue,js,ts}`,
+		`./layouts/**/*.vue`,
+		`./pages/**/*.vue`,
+		`./composables/**/*.{js,ts}`,
+		`./plugins/**/*.{js,ts}`,
+		`./utils/**/*.{js,ts}`,
+		`./App.{js,ts,vue}`,
+		`./app.{js,ts,vue}`,
+		`./Error.{js,ts,vue}`,
+		`./error.{js,ts,vue}`,
+		`./app.config.{js,ts}`,
+		`./nuxt.config.{js,ts}`,
+		`./form.theme.js`,
+		`./formkit.config.{js,ts}`,
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
 
